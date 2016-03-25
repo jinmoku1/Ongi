cf9b9f02537894e974ecc9b1ffc564191e2403e9
andew(주영광) <andew.ju@coupang.com>
2016-03-26 04:42:06 +0900

    insert usage

---
+++ Web/controls/utils/calculate.js
@@ -0,0 +1,12 @@
+var ongiCluster = (function(){
+	var data = [[2,6], [3,4], [3,8]];
+
+	var exports = {};
+
+	exports.init = function(){
+	};
+
+	return exports;
+})();
+
+exports.ongiCluster = ongiCluster;
